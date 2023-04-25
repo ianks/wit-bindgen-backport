c1b2c17e14f8249cd0aa90ce01047a4ef74aadac
Ian Ker-Seymer <ian.kerseymer@shopify.com>
2023-04-25 17:38:58 -0400

    Use repr(u8) when generating record structs

--- crates/gen-rust/src/lib.rs
+++ crates/gen-rust/src/lib.rs
@@ -508,12 +508,11 @@ pub trait RustGenerator {
             let lt = self.lifetime_for(&info, mode);
             self.rustdoc(docs);
             if !info.owns_data() {
-                self.push_str("#[repr(u8)]\n");
                 self.push_str("#[derive(Copy, Clone)]\n");
             } else if !info.has_handle {
-                self.push_str("#[repr(C)]\n");
                 self.push_str("#[derive(Clone)]\n");
             }
+            self.push_str("#[repr(u8)]\n");
             self.push_str(&format!("pub struct {}", name));
             self.print_generics(&info, lt, true);
             self.push_str(" {\n");
